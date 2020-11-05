e12a4e10b3c2e6bf107cb5f93f0ad2b563d2fb45
Cory Potter <pottercory22@gmail.com>
2020-11-05 17:31:15 -0600

    loaded data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,18 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
-data.forEach(ufoSightings => {
-    var row = 
+// Get a reference to the table body using d3
+var tBody = d3.select('#ufo-table>tbody');
+
+
+// Loop through each ufoSighting
+tableData.forEach((ufoSighting) => {
+    // append a row to the table body
+    var row =  tBody.append('tr');
+    // loop through each value in the object and append that value to the table
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
 })
+

cdd69064ddfbb7d87f9a5f533ac8e68f19046a4e
Cory Potter <pottercory22@gmail.com>
2020-11-08 18:15:40 -0600

    added city filter, createTable function, reset btn

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,19 +5,27 @@ var tableData = data;
 var tBody = d3.select('#ufo-table>tbody');
 
 // Create Table
-// Loop through each ufoSighting
-tableData.forEach((ufoSighting) => {
-    // append a row to the table body
-    var row =  tBody.append('tr');
-    // loop through each value in the object and append that value to the table
-    Object.entries(ufoSighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+function createTable(data) {
+    // Loop through each ufoSighting
+    data.forEach((ufoSighting) => {
+        // append a row to the table body
+        var row =  tBody.append('tr');
+        // loop through each value in the object and append that value to the table
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        })
     })
-})
+}
 
-// Select the button
-var button = d3.select("#button");
+// create default overall table
+createTable(tableData)
+
+// Select the filter button
+var button = d3.select("#filter-button");
+
+// select the reset button
+var reset = d3.select('#reset-button')
 
 // Select the form
 var form = d3.select("#ufo-form");
@@ -25,6 +33,7 @@ var form = d3.select("#ufo-form");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
+reset.on("click", runReset)
 
 
 // Complete the event handler function for the form
@@ -33,50 +42,52 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#witness-form-input");
+    // Select the date element and get the raw HTML node
+    var dateElement = d3.select("#datetime-form-input");
   
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    // Get the value property of the date element
+    var dateValue = dateElement.property("value");
   
-    console.log(inputValue);
-    console.log(tableData);
-
-    // filter by user input datetime
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue)
+    console.log(dateValue);
     
-    console.log(filteredData);
+    // Select the date element and get the raw HTML node
+    var cityElement = d3.select("#city-form-input");
+  
+    // Get the value property of the date element
+    var cityValue = cityElement.property("value").toLowerCase().trim();
+  
+    console.log(cityValue);
 
     // clear the table body, so we can return the filtered data
     tBody.html("")
     
-    // Create date filter
-    if (inputValue === '') {
-        // Create Overall Table if no date is entered
-        // Loop through each ufoSighting
-        tableData.forEach((ufoSighting) => {
-            // append a row to the table body
-            var row =  tBody.append('tr');
-            // loop through each value in the object and append that value to the table
-            Object.entries(ufoSighting).forEach(([key, value]) => {
-                var cell = row.append("td");
-                cell.text(value);
-            })
-        })
+    // create filtered table
+    if (dateValue != '') {
+        // filter by user input datetime
+        var filteredData = tableData.filter(sighting => sighting.datetime == dateValue);
     }
 
-    else {
-        // Create filtered table
-        // Loop through each filtered ufoSighting
-        filteredData.forEach((ufoSighting) => {
-            // append a row to the table body
-            var row =  tBody.append('tr');
-            // loop through each filtered result and append that value to the table
-            Object.entries(ufoSighting).forEach(([key, value]) => {
-                var cell = row.append("td");
-                cell.text(value);
-            })
-        })
+    if (cityValue != '') {
+        // filter by city
+        var filteredData = tableData.filter(data => data.city === cityValue);
     }
+
+    console.log(filteredData)
+    createTable(filteredData)
+}
+
+
+function runReset() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // clear the table body
+    tBody.html("")
+    // reset to overall table
+    createTable(tableData)
+    // clear datetime filter input
     
+    // clear city filter input
+
+    // cityElement.val('')
+    console.log("Reset the Table")
 }
